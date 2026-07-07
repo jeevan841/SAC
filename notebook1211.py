15259ba3f301d42b5faeaf15a34216eff4c83340
Jeevan Palle <169912411+jeevan841@users.noreply.github.com>
2026-07-07 23:43:47 +0530

    Update notebook1211.py

--- notebook1211.py
+++ notebook1211.py
@@ -97,7 +97,7 @@ In[16]:
 numpy.arange(0,2.2,0.3)
 
 
-# In[17]:
+In[17]:
 
 
 numpy.linspace(0,2,10)
@@ -105,56 +105,56 @@ numpy.linspace(0,2,10)
 
 # stop is inclusive in linspace but not in arange
 
-# In[18]:
+In[18]:
 
 
 a = numpy.arange(6)
 
 
-# In[19]:
+In[19]:
 
 
 a
 
 
-# In[20]:
+In[20]:
 
 
 a[4]
 
 
-# In[21]:
+In[21]:
 
 
 a[2:5]
 
 
-# In[22]:
+In[22]:
 
 
 a[:4:2] = -999
 
 
-# In[23]:
+In[23]:
 
 
 a
 
 
-# In[24]:
+In[24]:
 
 
 a[::-1]
 
 
-# In[25]:
+In[25]:
 
 
 for j in a:
     print(j)
 
 
-# In[26]:
+In[26]:
 
 
 a = numpy.array([[1,2,3,4],
@@ -163,90 +163,77 @@ a = numpy.array([[1,2,3,4],
 ])
 
 
-# In[27]:
+In[27]:
 
 
 a[2,3]
 
 
-# In[28]:
+In[28]:
 
 
 a[:2, 1:3]
 
 
-# In[29]:
+In[29]:
 
 
 lower_axes=a[1:2, :]
 
 
-# In[30]:
+In[30]:
 
 
 lower_axes
 
 
-# In[31]:
+In[31]:
 
 
 lower_axes.ndim
 
 
-# In[32]:
+In[32]:
 
 
 same_axes=a[1:2,:]
 
 
-# In[33]:
+In[33]:
 
 
 same_axes
 
 
-# In[34]:
+In[34]:
 
 
 a[:,1]
 
 
-# In[35]:
+In[35]:
 
 
 a[:,1:2]
 
 
-# In[36]:
+In[36]:
 
 
 a[:,1:3]
 
 
-# In[37]:
+In[37]:
 
 
 for row in a:
     print(row)
 
 
-# In[38]:
+In[38]:
 
 
 for r in a.flat:
     print(r)
-
-
-# #.flat function helps to make multidimentional array behave like 1-dimentional array for iteration
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
